e595bdcf4f148bd89d4005b7b925980da555bf86
zaher <zaherabdulazeez@gmail.com>
2016-04-04 10:06:47 +0530

    For first push, location,explorer,data modules started off

---
+++ bella_data.py
@@ -0,0 +1,27 @@
+#Authors: Zaher Abdul Azeez
+""" All the data structure related classes of bella are implemented here. 
+Bella has results in Dicts and classes related to manipulating Dicts are here.
+Dicts are converted to JSON to handle over to javascript
+JSON handling classes are implemnted here
+"""
+import json
+
+
+
+class MasterDataFrame(object):
+	"""Master Data frame: List of dictionaries to be converted to JSON.
+	Instantiate with a list of keys that the dict would have
+	"""
+	def __init__(self,keylist):
+		self.sub_dict = dict.fromkeys(keylist,)
+
+	def add_key(key):
+
+
+
+
+class JSONObject(object):
+	"""The full json data frame where data from all the bella classes are aggregated"""
+	def __init__(self, arg):
+		
+
---
+++ keys.py
@@ -0,0 +1,4 @@
+import os
+
+GMAPS_JS_KEY = os.environ['GMAPS_JS_KEY']
+GMAPS_SERVER_KEY = os.environ['GMAPS_SERVER_KEY']
