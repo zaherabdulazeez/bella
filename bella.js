6cb82dde7b3c51fd3e5bd7b42ac0a65233d8a79e
zaher <zaherabdulazeez@gmail.com>
2016-04-07 06:24:54 +0530

    working heat map and interactive sliders.Dirty hacks.

--- bella.js
+++ bella.js
@@ -7,4 +7,10 @@
     $('#food').slider()
     $('#health').slider()
     $('#education').slider()
+    $('.span2').on('slide', function (ev) {
+        var f = parseInt($('#food').val());
+        var h = parseInt($('#health').val());
+        var e = parseInt($('#education').val());
+        reload(f,h,e);
+});
 });
